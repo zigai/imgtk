413a6931a85af3d92ae9e65c531b1f5b7af7e190
zigai <ziga.ivansek@gmail.com>
2023-03-24 15:40:12 +0100

    nicer imports

--- imgtk/__init__.py
+++ imgtk/__init__.py
@@ -1,4 +1,7 @@
+from pypipeline import Pipeline, PriorityPipeline
+
 from imgtk import filters, transformers
 from imgtk.cli import cli
 from imgtk.filters import FILTERS_MAPPING
 from imgtk.img import ImageItem
+from imgtk.transformers import TRANSFORMERS_MAPPING
