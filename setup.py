d198a80bb93f5ee4a89a50691cada39c0e6f0291
zigai <ziga.ivansek@gmail.com>
2023-10-09 17:56:36 +0200

    update requirements

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ REQUIREMENTS = (HERE / "requirements.txt").read_text()
 
 setup(
     name="imgtk-python",
-    version="0.0.4",
+    version="0.0.5",
     description="",
     long_description=README,
     long_description_content_type="text/markdown",
